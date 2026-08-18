d5ea9b3b613c98b84f0c92c1545ffad693be55a9
Pirha <pirhamahar357@gmail.com>
2026-08-18 15:04:24 +0500

    Validation added

--- src/Student.java
+++ src/Student.java
@@ -5,9 +5,12 @@ class Student{
   public static void main(String){
    Scanner sc=new Scanner(System.in);
    void  Studetail(String name,String roll_no){
-                   
+        if(uni=="IBA"){
        System.out.println("Name: "+name);
        System.out.println("CMS: "+roll_no);
+    }
+   else
+    System.out.println("No record found!");
     }
     System.out.println("Enter name:");
    String name=sc.nextLine();
